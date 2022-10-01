52ac118dffe933b8338a32eae7b919ad04f7627c
vanderleiks <vanderleiksilva@gmail.com>
2022-10-01 19:41:13 -0300

    create project and add Category class

---
+++ src/test/java/dscatalog/NewDsCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewDsCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
